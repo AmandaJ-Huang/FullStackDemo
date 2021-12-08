1bca144fe22fa3af18f5180a4220c582becd0474
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-12-07 21:57:51 -0500

    PutMapping, DeleteMapping in StudentController

--- src/main/java/controller/StudentController.java
+++ src/main/java/controller/StudentController.java
@@ -9,9 +9,11 @@ import com.zipcode.FullStackDemo.entity.Student;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,6 +47,24 @@ public class StudentController {
         Student savedStudent = studentRepo.save(student);
         return ResponseEntity.created(new URI("/students" 
                     + savedStudent.getId())).body(savedStudent);
+    }
+
+    @PutMapping("/(id)")
+    public ResponseEntity<Student> updateStudent(@PathVariable Long id, @RequestBody Student student) {
+        log.info("Request to update student");
+        Student studentToUpdate = studentRepo.findById(id).orElseThrow(RuntimeException::new);
+        studentToUpdate.setFirstName(student.getFirstName());
+        studentToUpdate.setLastName(student.getLastName());
+        studentToUpdate.setCohort(student.getCohort());
+        studentRepo.save(studentToUpdate);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/(id)")
+    public ResponseEntity<?> deleteStudent(@PathVariable Long id) {
+        log.info("Request to delete student");
+        studentRepo.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 
 }
